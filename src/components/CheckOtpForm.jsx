106542143f191ccb2239f231186c01424215695f
hossein <hosseinbakhtiari781@outlook.com>
2024-06-01 12:52:11 +0330

    get otp code and check the correctness and get this message 'your login successfully'

--- src/components/CheckOtpForm.jsx
+++ src/components/CheckOtpForm.jsx
@@ -1,15 +1,25 @@
 import React from "react";
+import { checkOtp } from "../services/auth";
 
 function CheckOtpForm({ code, setCode, mobile, setStep }) {
+  const submitHandelr = async (event) => {
+    event.preventDefault();
+    console.log({ code, mobile });
 
-    const submitHandelr = (event) => {
-        event.preventDefault();
-        console.log({code , mobile})
+    if (code.length !== 5) return;
+
+    const { response, error } = await checkOtp(mobile, code);
+    if (response) {
+      console.log(response);
     }
+    if (error) {
+      console.log(error.response.data.message);
+    }
+  };
 
   return (
-    <form onSubmit={submitHandelr} >
-        <h1>Hi</h1>
+    <form onSubmit={submitHandelr}>
+      <h1>Hi</h1>
       <p>تایید کد</p>
       <span>کد ارسال شده به شماره "{mobile}" را وارد کنید</span>
       <label htmlFor="input">کد تأیید را وارد کنید</label>
