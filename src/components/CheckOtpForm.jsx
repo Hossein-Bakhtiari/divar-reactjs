f4c3cdaf55798f11e3623db7f645a78d9cb3bc19
hossein <hosseinbakhtiari781@outlook.com>
2024-06-01 18:33:29 +0330

    save the accessToken and refreshToken in the cookei

--- src/components/CheckOtpForm.jsx
+++ src/components/CheckOtpForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { checkOtp } from "../services/auth";
+import { setCookei } from "../utils/cookie";
 
 function CheckOtpForm({ code, setCode, mobile, setStep }) {
   const submitHandelr = async (event) => {
@@ -11,6 +12,7 @@ function CheckOtpForm({ code, setCode, mobile, setStep }) {
     const { response, error } = await checkOtp(mobile, code);
     if (response) {
       console.log(response);
+      setCookei(response.data);
     }
     if (error) {
       console.log(error.response.data.message);
@@ -19,7 +21,6 @@ function CheckOtpForm({ code, setCode, mobile, setStep }) {
 
   return (
     <form onSubmit={submitHandelr}>
-      <h1>Hi</h1>
       <p>تایید کد</p>
       <span>کد ارسال شده به شماره "{mobile}" را وارد کنید</span>
       <label htmlFor="input">کد تأیید را وارد کنید</label>
